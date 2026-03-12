ee40895930521d83df5c7c763efdfabbd6e1bd86
HwangLongg <longlongokok2@gmail.com>
2026-03-12 09:12:03 +0700

    Update Bai4.java
    Added a method to evaluate password strength based on criteria.

--- Bai4.java
+++ Bai4.java
@@ -1,23 +1,37 @@
-import java.util.Scanner;
+
 
 public class Bai4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập mã thẻ: ");
-        String maThe = sc.nextLine();
 
-        // Regex: 2 chữ hoa + 4 số năm + 5 số ngẫu nhiên
-        String mauRegex = "^[A-Z]{2}\\d{4}\\d{5}$";
+    public String evaluatePasswordStrength(String password) {
 
-        if (maThe.matches(mauRegex)) {
-            System.out.println("Mã thẻ hợp lệ!");
-        } else {
-            // Kiểm tra từng đoạn để báo lỗi cụ thể
-            if (!maThe.startsWith("TV")) {
-                System.out.println("Lỗi: Phải bắt đầu bằng TV");
-            } else {
-                System.out.println("Lỗi: Sai định dạng năm hoặc số ngẫu nhiên");
-            }
+        if (password == null || password.length() < 8) {
+            return "Yếu";
         }
+
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
+
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c))
+                hasUpper = true;
+            else if (Character.isLowerCase(c))
+                hasLower = true;
+            else if (Character.isDigit(c))
+                hasDigit = true;
+            else
+                hasSpecial = true;
+        }
+
+        if (hasUpper && hasLower && hasDigit && hasSpecial) {
+            return "Mạnh";
+        }
+
+        if ((hasUpper || hasLower) && (hasDigit || hasSpecial)) {
+            return "Trung bình";
+        }
+
+        return "Yếu";
     }
 }
